9846bdd89c825e014a53f83a40b7d013687625e6
siham-mahmoud <sihammahmoud220@gmail.com>
2024-07-21 02:57:29 +0300

    inital commit

---
+++ app1/src/Components/About/About.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function about() {
+  return (
+    <div><h3 className='bg-sky-300'>hello from about</h3></div>
+  )
+}
---
+++ app1/src/Components/Home/Home.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div ><h1 className="bg-red-200" >welcome from home</h1></div>
+  )
+}
